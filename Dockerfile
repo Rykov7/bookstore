37329967dbdc697c5bd17139afa4a97a3d8bcfd9
Rykov7 <rykov7@protonmail.ch>
2023-04-04 23:12:57 +0300

    ch10

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,18 @@ FROM python:3.11.2-slim-bullseye
 
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
+ENV LANG ru_RU.UTF-8
 
 WORKDIR /code
 
 COPY ./requirements.txt .
+
+#RUN echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+#RUN echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+#RUN echo "LANG=en_US.UTF-8" > /etc/locale.conf
+#RUN apt-get clean && apt-get update && apt-get install -y locales
+#RUN locale-gen en_US.UTF-8
+
 RUN python -m pip install --upgrade pip
 RUN pip install -r requirements.txt
 
--- django_project/settings.py
+++ django_project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
 
 TIME_ZONE = "UTC"
 
@@ -143,10 +143,20 @@ AUTHENTICATION_BACKENDS = (
     "django.contrib.auth.backends.ModelBackend",
     "allauth.account.auth_backends.AuthenticationBackend",
 )
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+#
 ACCOUNT_SESSION_REMEMBER = True
 ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
+
+# email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+DEFAULT_FROM_EMAIL = "rykov7ru@gmail.com"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
